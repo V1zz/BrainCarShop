96a79ec21dc49ff973573e7df29faf7c2c986a76
V1zz <chumachenkoilya@gmail.com>
2017-04-05 22:02:52 +0300

    bad home form

--- BrainCarShop/Controllers/HomeController.cs
+++ BrainCarShop/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
-namespace BrainCarShop.Controllers
+namespace IdentitySample.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
-            ViewBag.Title = "Car Shop";
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
             return View();
         }
     }
